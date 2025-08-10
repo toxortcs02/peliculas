928d51c2f61689daeb9f519ac059a85589f3a38d
Tomas Sabella <tomassabella@hotmail.com>
2025-08-10 03:18:38 -0300

    Refactor App component to utilize PageWrapper and update Movie component props
    - Removed unnecessary login and signup forms from App.js
    - Introduced PageWrapper component to encapsulate layout and structure
    - Updated Movie component to accept props for dynamic rendering of movie details
    - Added sample movie data directly in App.js for rendering

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,14 @@
 export default function Movie(props){
     return (
     <div className="movie-item-style-2">
-			<img src="images/uploads/mv1.jpg" alt=""/>
+			<img src={props.img} alt=""/>
 			<div className="mv-item-infor">
 				<h6><a href="moviesingle.html">{props.title}</a></h6>
 						<p className="rate"><i className="ion-android-star"></i><span>{props.score}</span> /10</p>
-						<p className="describe">{props.description}</p>
-						<p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
-						<p>Director: <a href="#">Joss Whedon</a></p>
-						<p>Stars: <a href="#">Robert Downey Jr.,</a> <a href="#">Chris Evans,</a> <a href="#">  Chris Hemsworth</a></p>
+						<p className="describe">{props.children}</p>
+						<p className="run-time"> {props.duration}    -     <span>{props.mmpa} </span>    -     <span>{props.release}</span></p>
+						<p>Director: <a href="#">{props.director}</a></p>
+						<p>Stars: <a href="#">{props.stars}</a></p>
 			</div>
     </div>
     )
